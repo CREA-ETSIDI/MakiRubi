b0f2e5e9960be48f939db8ebcb2c0d7a3fb8bfa2
Marth98 <47816517+martinrteran@users.noreply.github.com>
2022-03-10 23:09:57 +0100

    Tratamiento de lo pasado a arduino
    En el caso de que se escriban los movimientos como 'R', y 'R_'
    Se convertirán las 'R_' a 'r' y se enviarán en este formato

--- ComunicacionArduino.py
+++ ComunicacionArduino.py
@@ -13,13 +13,38 @@ class ArduinoCom(object):
         self.ComHandler=serial.Serial(self.port,self.baudios)
 
     def write(self, info):
-        # The info has to be a string, here it would be treated7
+        # The info has to be a string, here it would be treated
+        top=len(info)
+        # if the data type is a string
+        if type(info)==type(str()):
+            info=info.upper()
+            string=str()
+            # First convert to lower cases the ones that are followed by '_'
+            if type(info)==type(str()):
+            info=info.upper()
+            string=str()
+            # First convert to lower cases the ones that are followed by '_'
+            for i in range(0,top,1):
+                if info[i]!='_':
+                    if (i+1)<top:
+                        if info[i+1]=='_':
+                            string+=info[i].lower()
+                        else:
+                            string+=info[i]
+                    else:
+                        string+=info[i]
+                            #info[i]=info[i].lower()
+            # It sends only the letters
+            print('Before Sending.  '+string)
+        for i in string:
+            self.ComHandler.write(i.encode('ascii')) # return the amount of sent data
+        
         for i in info:
             return self.ComHandler.write(i.encode('ascii')) # return the amount of sent data
    
-    def read(self,Nobytes):
+    def read(self):
         # It reads the amount of bytes passed
-        return self.ComHandler.read(NoBytes)
+        return self.ComHandler.read_all().decode('ascii')
 
     def Close(self):
         # it closes the communication port
