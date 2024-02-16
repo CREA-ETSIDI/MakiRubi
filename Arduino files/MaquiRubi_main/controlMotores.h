4ec1688286531a29d6e400b6a8a5dbf21cb444a4
Leonel Aguilera <la.aguilera@alumnos.upm.es>
2024-02-16 12:23:56 +0100

    Limpieza del código de control de motores

--- Arduino files/MaquiRubi_main/controlMotores.h
+++ Arduino files/MaquiRubi_main/controlMotores.h
@@ -1,21 +1,10 @@
 #ifndef __controlMotores_h__
 #define __controlMotores_h__
 
-#include "controlSerial.h"
-
 #define __modo_normal__
 //#define __modo_alto_par__
 
-//enum SentidoGiro {Horario=0,AntiHorario=1};
-enum Motor {U=0,D,R,L,F,B};
-enum SentidoGiro {Horario=0,AntiHorario=1};
-
-extern int motorMatrix[6][4]; // 6 motores con 4 pines de control
-
-void keepOn(int motors[][4], Motor mantiene);
-void motorMatrix_init();
+enum motor_t {U, D, R, L, F, B};
+enum sentidogiro_t {Horario, AntiHorario};
 void secuenciaGiros(char cadena[], int tam);
-SentidoGiro getSentidoGiro(char instruccion);
-void giro (bool sentido,int pines[]);
-void secuenciaGiros(int MotorMatrix[][4],char cadena[],int tam);
 #endif
