61f7d19c633224558349ac46863a2a96bdb29bf8
LeonelAguilera <la.aguilera@alumnos.upm.es>
2024-02-17 08:51:14 +0100

    Añadida, básicamente, toda la funcionalidad del control de motores

--- Arduino files/MaquiRubi_main/controlMotores.cpp
+++ Arduino files/MaquiRubi_main/controlMotores.cpp
@@ -1,55 +1,320 @@
 #include "controlMotores.h"
 
-void secuenciaGiros(char cadena[], int tam)
+void secuenciaGiros(char cadena[], uint8_t tam)
 {
-  instruccion instrucciones[tam];
+  instruccion_t instrucciones[tam];
   parsear_instrucciones(instrucciones, cadena, tam);
+  ejecutar_instrucciones(instrucciones, tam);
 }
 
-instruccion map_char_to_instruccion(char comando)
+instruccion_t map_char_to_instruccion(char comando)
 {
   switch(comando)
   {
     case 'U':
-      return {U, Horario};
+      return {
+        .motor = {
+            .IN1 = U_IN1,
+            .IN2 = U_IN2,
+            .IN3 = U_IN3,
+            .IN4 = U_IN4,
+            .ENA = U_ENA,
+            .ENB = U_ENB,
+            .curr_pos = 0
+            },
+        .sentido = Horario
+        };
     case 'D':
-      return {D, Horario};
+      return {
+        .motor = {
+            .IN1 = D_IN1,
+            .IN2 = D_IN2,
+            .IN3 = D_IN3,
+            .IN4 = D_IN4,
+            .ENA = D_ENA,
+            .ENB = D_ENB,
+            .curr_pos = 0
+            },
+        .sentido = Horario
+        };
     case 'R':
-      return {R, Horario};
+      return {
+        .motor = {
+            .IN1 = R_IN1,
+            .IN2 = R_IN2,
+            .IN3 = R_IN3,
+            .IN4 = R_IN4,
+            .ENA = R_ENA,
+            .ENB = R_ENB,
+            .curr_pos = 0
+            },
+        .sentido = Horario
+        };
     case 'L':
-      return {L, Horario};
+      return {
+        .motor = {
+            .IN1 = L_IN1,
+            .IN2 = L_IN2,
+            .IN3 = L_IN3,
+            .IN4 = L_IN4,
+            .ENA = L_ENA,
+            .ENB = L_ENB,
+            .curr_pos = 0
+            },
+        .sentido = Horario
+        };
     case 'F':
-      return {F, Horario};
+      return {
+        .motor = {
+            .IN1 = F_IN1,
+            .IN2 = F_IN2,
+            .IN3 = F_IN3,
+            .IN4 = F_IN4,
+            .ENA = F_ENA,
+            .ENB = F_ENB,
+            .curr_pos = 0
+            },
+        .sentido = Horario
+        };
     case 'B':
-      return {B, Horario};
+      return {
+        .motor = {
+            .IN1 = B_IN1,
+            .IN2 = B_IN2,
+            .IN3 = B_IN3,
+            .IN4 = B_IN4,
+            .ENA = B_ENA,
+            .ENB = B_ENB,
+            .curr_pos = 0
+            },
+        .sentido = Horario
+        };
     case 'u':
-      return {U, AntiHorario};
+      return {
+        .motor = {
+            .IN1 = U_IN1,
+            .IN2 = U_IN2,
+            .IN3 = U_IN3,
+            .IN4 = U_IN4,
+            .ENA = U_ENA,
+            .ENB = U_ENB,
+            .curr_pos = 0
+            },
+        .sentido = AntiHorario
+        };
     case 'd':
-      return {D, AntiHorario};
+      return {
+        .motor = {
+            .IN1 = D_IN1,
+            .IN2 = D_IN2,
+            .IN3 = D_IN3,
+            .IN4 = D_IN4,
+            .ENA = D_ENA,
+            .ENB = D_ENB,
+            .curr_pos = 0
+            },
+        .sentido = AntiHorario
+        };
     case 'r':
-      return {R, AntiHorario};
+      return {
+        .motor = {
+            .IN1 = R_IN1,
+            .IN2 = R_IN2,
+            .IN3 = R_IN3,
+            .IN4 = R_IN4,
+            .ENA = R_ENA,
+            .ENB = R_ENB,
+            .curr_pos = 0
+            },
+        .sentido = AntiHorario
+        };
     case 'l':
-      return {L, AntiHorario};
+      return {
+        .motor = {
+            .IN1 = L_IN1,
+            .IN2 = L_IN2,
+            .IN3 = L_IN3,
+            .IN4 = L_IN4,
+            .ENA = L_ENA,
+            .ENB = L_ENB,
+            .curr_pos = 0
+            },
+        .sentido = AntiHorario
+        };
     case 'f':
-      return {F, AntiHorario};
+      return {
+        .motor = {
+            .IN1 = F_IN1,
+            .IN2 = F_IN2,
+            .IN3 = F_IN3,
+            .IN4 = F_IN4,
+            .ENA = F_ENA,
+            .ENB = F_ENB,
+            .curr_pos = 0
+            },
+        .sentido = AntiHorario
+        };
     case 'b':
-      return {B, AntiHorario};
+      return {
+        .motor = {
+            .IN1 = B_IN1,
+            .IN2 = B_IN2,
+            .IN3 = B_IN3,
+            .IN4 = B_IN4,
+            .ENA = B_ENA,
+            .ENB = B_ENB,
+            .curr_pos = 0
+            },
+        .sentido = AntiHorario
+        };
     default:
-      return {Null, Horario}; //POR QUÉ TENGO QUE HACER ESTO ASÍ? QUIERO PROGRAMARLO EN RUST
+      return {
+        .motor = {
+            .IN1 = 0,
+            .IN2 = 0,
+            .IN3 = 0,
+            .IN4 = 0,
+            .ENA = 0,
+            .ENB = 0,
+            .curr_pos = 0
+            },
+        .sentido = Stop
+        }; //POR QUÉ TENGO QUE HACER ESTO ASÍ? QUIERO PROGRAMARLO EN RUST
       //QUE ALGUIEN ME DE UN Option<T> POR FAVOR
       //MUERTE A LOS NULL!
   }
 }
 
-int parsear_instrucciones(instruccion instrucciones[], char cadena[], int tam)
+int8_t parsear_instrucciones(instruccion_t instrucciones[], char cadena[], uint8_t tam)
 {
-    for (int i = 0; i < tam; i++)
+    for (uint8_t i = 0; i < tam; i++)
     {
         instrucciones[i] = map_char_to_instruccion(cadena[i]);
-        if(instrucciones[i].motor == Null)
+        if(instrucciones[i].sentido == Stop)
         {
             return 0;
         }
     }
     return -1;
+}
+
+void ejecutar_instrucciones(instruccion_t instrucciones[], uint8_t tam)
+{
+    for (uint8_t i = 0; i < tam; i++)
+    {
+        if(instrucciones[i].sentido == Stop)
+        {
+            return;
+        }
+        mover_motor_90(instrucciones[i]);
+    }
+}
+
+void mover_motor_90(instruccion_t instruccion)
+{
+    static bool pasos[4][4] = {
+        {1, 0, 1, 0},
+        {1, 0, 0, 1},
+        {0, 1, 0, 1},
+        {0, 1, 1, 0}  
+    };
+    constexpr uint16_t periodo = 60000000.0/(RPM*200.0);
+
+    for(uint8_t i = 0; i < 50; i++)
+    {
+        if(instruccion.sentido == Horario)
+        {
+            instruccion.motor.curr_pos++;   
+        }
+        else
+        {
+            instruccion.motor.curr_pos--;
+        }
+        instruccion.motor.curr_pos &= 0b00000011;
+
+        digitalWrite(instruccion.motor.IN1, pasos[instruccion.motor.curr_pos][0]);
+        digitalWrite(instruccion.motor.IN2, pasos[instruccion.motor.curr_pos][1]);
+        digitalWrite(instruccion.motor.IN3, pasos[instruccion.motor.curr_pos][2]);
+        digitalWrite(instruccion.motor.IN4, pasos[instruccion.motor.curr_pos][3]);
+
+        delayMicroseconds(periodo);
+    }
+}
+
+//Control de corriente
+//U
+ISR(INT0_vect){
+    OCR0A = TCNT0;
+}
+ISR(INT1_vect){
+    OCR0B = TCNT0;
+}
+//TIMER0_OVF_vect está ocupado por Arduino.h para actualizar millis().
+//Probablemente prescinda de la Arduino.h en futuras versiones
+ISR(TIMER0_COMPA_vect){
+    OCR0A++;
+}
+ISR(TIMER0_COMPB_vect){
+    OCR0B++;
+}
+
+//D
+ISR(INT2_vect){
+    OCR1A = TCNT1;
+}
+ISR(INT3_vect){
+    OCR1B = TCNT1;
+}
+ISR(TIMER1_OVF_vect){
+    OCR1A++;
+    OCR1B++;
+}
+
+//R
+ISR(INT5_vect){
+    OCR2A = TCNT2;
+}
+ISR(PCINT1_vect){
+    OCR2B = TCNT1;
+}
+ISR(TIMER2_OVF_vect){
+    OCR2A++;
+    OCR2B++;
+}
+
+//L, F, B
+ISR(PCINT2_vect){
+    if(PINK & 0b00000001){
+        OCR3A = TCNT3;
+    }
+    if(PINK & 0b00000010){
+        OCR3B = TCNT3;
+    }
+    if(PINK & 0b00000100){
+        OCR4A = TCNT4;
+    }
+    if(PINK & 0b00001000){
+        OCR4B = TCNT4;
+    }
+    if(PINK & 0b00010000){
+        OCR5A = TCNT5;
+    }
+    if(PINK & 0b00100000){
+        OCR5B = TCNT5;
+    }
+}
+//L
+ISR(TIMER3_OVF_vect){
+    OCR3A++;
+    OCR3B++;
+}
+//F
+ISR(TIMER4_OVF_vect){
+    OCR4A++;
+    OCR4B++;
+}
+//B
+ISR(TIMER5_OVF_vect){
+    OCR5A++;
+    OCR5B++;
 }
--- Arduino files/MaquiRubi_main/controlMotores.h
+++ Arduino files/MaquiRubi_main/controlMotores.h
@@ -4,18 +4,85 @@
 #include <Arduino.h>
 #include "controlSerial.h"
 
-#define __modo_normal__
-//#define __modo_alto_par__
+#define RPM 350
 
-enum motor_t {Null, U, D, R, L, F, B};
-enum sentidogiro_t {Horario, AntiHorario};
+/*********************
+***PIN**DEFINITIONS*** 
+*********************/
+#define U_IN1 0
+#define U_IN2 0
+#define U_IN3 0
+#define U_IN4 0
+#define U_ENA 13
+#define U_ENB 4
+#define U_CMPA 21
+#define U_CMPB 20
+
+#define D_IN1 0
+#define D_IN2 0
+#define D_IN3 0
+#define D_IN4 0
+#define D_ENA 11
+#define D_ENB 12
+#define D_CMPA 19
+#define D_CMPB 18
+
+#define R_IN1 0
+#define R_IN2 0
+#define R_IN3 0
+#define R_IN4 0
+#define R_ENA 10
+#define R_ENB 9
+#define R_CMPA 15
+#define R_CMPB 14
+
+#define L_IN1 0
+#define L_IN2 0
+#define L_IN3 0
+#define L_IN4 0
+#define L_ENA 5
+#define L_ENB 2
+#define L_CMPA 62
+#define L_CMPB 63
+
+#define F_IN1 0
+#define F_IN2 0
+#define F_IN3 0
+#define F_IN4 0
+#define F_ENA 6
+#define F_ENB 7
+#define F_CMPA 64
+#define F_CMPB 65
+
+#define B_IN1 0
+#define B_IN2 0
+#define B_IN3 0
+#define B_IN4 0
+#define B_ENA 46
+#define B_ENB 45
+#define B_CMPA 66
+#define B_CMPB 67
+
+enum sentidogiro_t {Horario, AntiHorario, Stop};
+
+typedef struct{
+    uint8_t IN1;
+    uint8_t IN2;
+    uint8_t IN3;
+    uint8_t IN4;
+    uint8_t ENA;
+    uint8_t ENB;
+    uint8_t curr_pos;
+} motor_t;
 
 typedef struct{
   motor_t motor;
   sentidogiro_t sentido;
-} instruccion;
+} instruccion_t;
 
-void secuenciaGiros(char cadena[], int tam);
-instruccion map_char_to_instruccion(char comando);
-int parsear_instrucciones(instruccion instrucciones[], char cadena[], int tam);
+void secuenciaGiros(char cadena[], uint8_t tam);
+instruccion_t map_char_to_instruccion(char comando);
+int8_t parsear_instrucciones(instruccion_t instrucciones[], char cadena[], uint8_t tam);
+void ejecutar_instrucciones(instruccion_t instrucciones[], uint8_t tam);
+void mover_motor_90(instruccion_t instruccion);
 #endif
